06823ed0f0dc02f405c76ae84fed9669997e2f20
Mateusz Kuniczuk <mateuszkuniczuk98@gmail.com>
2022-12-25 23:06:01 +0100

    Plan out structure

--- src/main.py
+++ src/main.py
@@ -1,21 +1,6 @@
-import time
 
-def time_it(func):
+def main():
+    pass
 
-    def inner(*args, **kwargs):
-        start = time.time()
-        called_func = func(*args, **kwargs)
-        end = time.time()
-        print(f"Duration: {end-start}.")
-    
-    return inner
-
-@time_it
-def loopsy(iterations: int):
-    return sum([i for i in range(iterations)])
-
-result = loopsy(100_000)
-print(f"Sum is {result}")
-
-result = loopsy(100_000)
-print(f"Sum is {result}")
+if __name__ == "__main__":
+    main()
