6c41221ba2d29effbb854c0a5b860de8394f489d
Mateusz Kuniczuk <mateuszkuniczuk98@gmail.com>
2023-01-08 13:45:30 +0100

    Finis get_ingredients to return proper info

--- src/recipe_scraper.py
+++ src/recipe_scraper.py
@@ -2,14 +2,16 @@ import requests
 
 from bs4 import BeautifulSoup
 
+
 class RecipeScraper:
     """
     Scrap recipe page from Kwestia Smaku to find details about ingredients.
-    
+
     The RecipeScraper should only attempt to find ingredients information
     and should not process any additional details, such as steps to create a recipe
     or pictures.
     """
+
     DEFAULT_PARSER = "lxml"
 
     def __init__(self, recipe_url: str):
@@ -18,25 +20,31 @@ class RecipeScraper:
 
     def _get_recipe_page(self) -> str:
         recipe_page = requests.get(self.recipe_url)
+        # TODO check for error response codes
         return recipe_page.text
-    
-    def _extract_ingredients_details(self, recipe_text: str):
+
+    def _extract_ingredients_details(
+        self, recipe_text: str
+    ) -> dict[str, str | list[str]]:
         soup = BeautifulSoup(recipe_text, self.DEFAULT_PARSER)
-        # TODO return actual ingredients
-        # <div class="group-skladniki field-group-div"><h3><span>Składniki</span></h3>
-        # porcje: .field-name-field-ilosc-porcji
-        # skladniki: .field-name-field-skladniki
-        portions = soup.select(".field-name-field-ilosc-porcji")
-        ingredients = soup.select(".field-name-field-skladniki")
-        print(portions, ingredients)
-        return soup
+
+        portions: str = soup.select(".field-name-field-ilosc-porcji")[0].text.strip()
+        ingredients_elements = soup.select_one(".field-name-field-skladniki").select(
+            "li"
+        )
+        ingredients = [ingredient.text.strip() for ingredient in ingredients_elements]
+
+        return {"portions": portions, "ingredients": ingredients}
 
     def get_ingredients(self):
         recipe_text = self._get_recipe_page()
         ingredients_details = self._extract_ingredients_details(recipe_text)
-        # TODO
+        # TODO refactor this, maybe move exract_ingredients_details to here
         return ingredients_details
-    
+
+
 if __name__ == "__main__":
-    scraper = RecipeScraper("https://www.kwestiasmaku.com/kuchnia_polska/rosol/przepis.html")
+    scraper = RecipeScraper(
+        "https://www.kwestiasmaku.com/kuchnia_polska/rosol/przepis.html"
+    )
     soup = scraper.get_ingredients()
